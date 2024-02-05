13d8f1644560827ebf54c1f18b927aae176b0572
PontusPilatus <paulsson.pontus@gmail.com>
2024-02-05 10:27:08 +0100

    added yatzy score

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import numpy as np
 import pandas as pd
 
+
 def roll_dices(dice_values, reroll_indices):
     dice_values[reroll_indices] = np.random.randint(1, 7, size=len(reroll_indices))
     return dice_values
 
+
 def calculate_round_score(dice_values, target_number):
-    return np.sum(dice_values == target_number) * target_number
+    if np.all(dice_values == dice_values[0]):
+        return 50
+    else:
+        return np.sum(dice_values == target_number) * target_number
+
 
 def player_round(player_name, round_number):
-    dice_values = np.random.randint(1,7, size=5)
+    dice_values = np.random.randint(1, 7, size=5)
     print(f"{player_name}'s turn, Round {round_number}, focusing on {round_number}'s. Initial roll: {dice_values}")
+
+    if np.all(dice_values == dice_values[0]):
+        print("Yatzy! 50 points!")
+        return 50
 
     for attempt in range(2):
         reroll_input = input("Enter the indices of dice to reroll (0-4), seperate by spaces, or press enter to keep: ")
@@ -20,8 +30,13 @@ def player_round(player_name, round_number):
         dice_values = roll_dices(dice_values, reroll_indices)
         print(f"New roll: {dice_values}")
 
+        if np.all(dice_values == dice_values[0]):
+            print("Yatzy! 50 points!")
+            return 50
+
     round_score = calculate_round_score(dice_values, round_number)
     return round_score
+
 
 def game():
     player1_name = input("Enter Player 1's name: ")
